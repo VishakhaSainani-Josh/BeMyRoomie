2851e8c4390b9de7663f6a9b56572449101e4b5a
VishakhaSainani <vishakhasainani26@gmail.com>
2025-02-12 14:32:31 +0530

    Add property service

--- internal/app/dependencies.go
+++ internal/app/dependencies.go
@@ -3,19 +3,25 @@ package app
 import (
 	"database/sql"
 
+	"github.com/VishakhaSainani-Josh/BeMyRoomie/internal/app/properties"
 	"github.com/VishakhaSainani-Josh/BeMyRoomie/internal/app/users"
 	"github.com/VishakhaSainani-Josh/BeMyRoomie/internal/repo"
 )
 
 type dependencies struct {
-	userService users.Service
+	userService     users.Service
+	propertyService properties.Service
 }
 
-//Start services
+// Start services
 func InitServices(db *sql.DB) dependencies {
 	userRepo := repo.NewUserRepo(db)
-
 	userService := users.NewService(userRepo)
 
-	return dependencies{userService: userService}
+	propertyRepo := repo.NewPropertyRepo(db)
+	propertyService := properties.NewService(propertyRepo)
+
+	return dependencies{userService: userService,
+		propertyService: propertyService}
+
 }
