1e5e81e928ffa0b32b85aff0e332c35f85f7e0ab
VishakhaSainani <vishakhasainani26@gmail.com>
2025-02-12 14:29:22 +0530

    Add authorization middleware

--- internal/pkg/middleware/middleware.go
+++ internal/pkg/middleware/middleware.go
@@ -6,23 +6,24 @@ import (
 	"strings"
 
 	constant "github.com/VishakhaSainani-Josh/BeMyRoomie/internal/pkg/constants"
+	"github.com/VishakhaSainani-Josh/BeMyRoomie/internal/pkg/errhandler"
 	"github.com/VishakhaSainani-Josh/BeMyRoomie/internal/pkg/jwt"
 	"github.com/VishakhaSainani-Josh/BeMyRoomie/internal/pkg/response"
 )
 
-//Checks whether the user is valid, that is signed in before the user can access the next handler functionality
-func AuthMiddleware(next http.HandlerFunc) http.HandlerFunc {
+// Checks whether the user is valid, that is signed in before the user can access the next handler functionality
+func Authentication(next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		authHeader := r.Header.Get("Authorization")
 		if authHeader == "" {
-			response.HandleResponse(w, http.StatusUnauthorized, "unauthorized access")
+			response.HandleResponse(w, http.StatusUnauthorized, errhandler.ErrAuth)
 			return
 		}
 
 		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
 		token, err := jwt.ParseJWT(tokenString)
 		if err != nil {
-			response.HandleResponse(w, http.StatusUnauthorized, "unauthorized access")
+			response.HandleResponse(w, http.StatusUnauthorized, errhandler.ErrAuth)
 			return
 		}
 
@@ -31,6 +32,20 @@ func AuthMiddleware(next http.HandlerFunc) http.HandlerFunc {
 		role := token.Role
 		ctx = context.WithValue(ctx, constant.RoleKey, role)
 		r = r.WithContext(ctx)
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+// Checks whether the user is authorized to perform an action, if role of user is lister then it allows property methods
+func Authorization(next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		role := ctx.Value(constant.RoleKey).(string)
+		if role != "lister" {
+			response.HandleResponse(w, http.StatusUnauthorized, errhandler.ErrAuth)
+			return
+		}
 
 		next.ServeHTTP(w, r)
 	})
