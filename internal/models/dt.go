8c4debf5594cbae075832e08321e46db3159fe6d
VishakhaSainani <vishakhasainani26@gmail.com>
2025-02-18 20:34:44 +0530

    Define Validations and corresponding errors

--- internal/models/dt.go
+++ internal/models/dt.go
@@ -1,8 +1,8 @@
 package models
 
 type LoginRequest struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password"  validate:"required,min=8,max=20"`
 }
 
 type LoginResponse struct {
@@ -11,13 +11,13 @@ type LoginResponse struct {
 }
 
 type NewUserRequest struct {
-	Name            string `json:"name"`
-	Phone           string `json:"phone"`
-	Email           string `json:"email"`
-	Password        string `json:"password"`
-	Gender          string `json:"gender"`
-	Role            string `json:"role"`
-	RequiredVacancy int    ` json:"required_vacancy"`
+	Name            string `json:"name" validate:"required,min=1,max=100"`
+	Phone           string `json:"phone" validate:"required,len=10"`
+	Email           string `json:"email" validate:"required,email"`
+	Password        string `json:"password" validate:"required,min=8,max=20"`
+	Gender          string `json:"gender" validate:"required,oneof=Male Female"  `
+	Role            string `json:"role" `
+	RequiredVacancy int    ` json:"required_vacancy" validate:"omitempty,gt=0"`
 }
 
 type UserResponse struct {
@@ -26,54 +26,58 @@ type UserResponse struct {
 }
 
 type NewPreferenceRequest struct {
-	City string   `json:"city"`
+	City string   `json:"city" validate:"required,alpha"`
 	Tags []string `json:"tags"`
 }
 
 type User struct {
 	UserId          int      `db:"user_id" json:"userId"`
-	Name            string   `db:"name" json:"name"`
-	Phone           string   `db:"phone" json:"phone"`
-	Email           string   `db:"email" json:"email"`
+	Name            string   `db:"name" json:"name" validate:"omitempty,min=2,max=100"`
+	Phone           string   `db:"phone" json:"phone" validate:"omitempty,len=10" `
+	Email           string   `db:"email" json:"email" validate:"omitempty,email"`
 	Password        string   `db:"passsword" json:"password"`
-	Gender          string   `db:"gender" json:"gender"`
-	City            string   `db:"city" json:"city"`
+	Gender          string   `db:"gender" json:"gender" validate:"omitempty,oneof=Male Female"`
+	City            string   `db:"city" json:"city" validate:"omitempty,alpha"`
 	Role            string   `db:"role" json:"role"`
-	RequiredVacancy int      `db:"required_vacancy" json:"required_vacancy"`
+	RequiredVacancy int      `db:"required_vacancy" json:"required_vacancy" validate:"omitempty,gt=0"`
 	Tags            []string `db:"tags" json:"tags"`
 }
 
 type Property struct {
 	PropertyId      int      `json:"property_id"`
 	ListerId        int      `json:"lister_id"`
-	Title           string   `json:"title"`
+	Title           string   `json:"title" validate:"omitempty,min=2,max=100"`
 	Description     string   `json:"description"`
-	City            string   `json:"city"`
+	City            string   `json:"city" validate:"omitempty,alpha"`
 	Address         string   `json:"address"`
 	Rent            int64    `json:"rent"`
 	Facilities      []string `json:"facilities"`
 	Images          []string `json:"images"`
-	PreferredGender string   `json:"preferred_gender"`
+	PreferredGender string   `json:"preferred_gender" validate:"omitempty,oneof=Male Female"`
 	Status          string   `json:"status"`
-	Vacancy         int      `json:"vacancy"`
+	Vacancy         int      `json:"vacancy" validate:"omitempty,gt=0"`
 }
 
 type NewPropertyRequest struct {
 	PropertyId      int      `json:"property_id"`
 	ListerId        int      `json:"lister_id"`
-	Title           string   `json:"title"`
+	Title           string   `json:"title" validate:"required,min=2,max=100"`
 	Description     string   `json:"description"`
-	City            string   `json:"city"`
-	Address         string   `json:"address"`
-	Rent            int64    `json:"rent"`
+	City            string   `json:"city" validate:"required,alpha"`
+	Address         string   `json:"address" validate:"required"`
+	Rent            int64    `json:"rent" validate:"required"`
 	Facilities      []string `json:"facilities"`
-	Images          []string `json:"images"`
-	PreferredGender string   `json:"preferred_gender"`
+	Images          []string `json:"images" validate:"required"`
+	PreferredGender string   `json:"preferred_gender" validate:"required,oneof=Male Female"`
 	Status          string   `json:"status"`
-	Vacancy         int      `json:"vacancy"`
+	Vacancy         int      `json:"vacancy" validate:"required,gt=0"`
 }
 
 type PropertyResponse struct {
 	PropertyId int    `json:"propertyId"`
 	Message    string `json:"message"`
+}
+
+type InterestStatusRequest struct {
+	IsAccepted bool `json:"is_accepted"`
 }
--- internal/pkg/errhandler/errors.go
+++ internal/pkg/errhandler/errors.go
@@ -6,17 +6,24 @@ import (
 )
 
 var (
-	Errbody           = errors.New("error reading body")
-	ErrInvalidReq     = errors.New("error invalid request")
-	ErrInternalServer = errors.New("internal server error")
-	ErrUserExist      = errors.New("email already registered")
-	ErrUserInvalid    = errors.New("invalid credentials")
-	ErrUserMissing    = errors.New("user doesn't exist")
-	ErrHash           = errors.New("password hashing error")
-	ErrToken          = errors.New("could not generate token")
-	ErrAuth           = errors.New("unauthorized access")
-	ErrExistProperty  = errors.New("already vacant property for user exists")
-	ErrPropertyAccess = errors.New("unauthorized to update property")
+	Errbody            = errors.New("error reading body")
+	ErrInvalidReq      = errors.New("error invalid request")
+	ErrInternalServer  = errors.New("internal server error")
+	ErrUserExist       = errors.New("email already registered")
+	ErrUserInvalid     = errors.New("invalid credentials")
+	ErrUserMissing     = errors.New("user doesn't exist")
+	ErrHash            = errors.New("password hashing error")
+	ErrToken           = errors.New("could not generate token")
+	ErrAuth            = errors.New("unauthorized access")
+	ErrExistProperty   = errors.New("already vacant property for user exists")
+	ErrPropertyAccess  = errors.New("unauthorized to update property")
+	ErrInvalidEmail    = errors.New("invalid email")
+	ErrInvalidPassword = errors.New("password length should be between 8 to 20")
+	ErrRequired        = errors.New("invalid details")
+	ErrInvalidPhone    = errors.New("number length should be 10")
+	ErrInvalidGender   = errors.New("gender should be Male or Female")
+	ErrInavlidVacancy  = errors.New("required vacancy should be greater than 0")
+	ErrInvalidTitle    = errors.New("title length should be atleast 2")
 )
 
 func MapError(err error) (int, string) {
@@ -43,6 +50,21 @@ func MapError(err error) (int, string) {
 		return http.StatusBadRequest, ErrPropertyAccess.Error()
 	case errors.Is(err, ErrAuth):
 		return http.StatusUnauthorized, ErrAuth.Error()
+	case errors.Is(err, ErrInvalidEmail):
+		return http.StatusUnprocessableEntity, ErrInvalidEmail.Error()
+	case errors.Is(err, ErrInvalidPassword):
+		return http.StatusUnprocessableEntity, ErrInvalidPassword.Error()
+	case errors.Is(err, ErrRequired):
+		return http.StatusUnprocessableEntity, ErrRequired.Error()
+	case errors.Is(err, ErrInvalidPhone):
+		return http.StatusUnprocessableEntity, ErrInvalidPhone.Error()
+	case errors.Is(err, ErrInvalidGender):
+		return http.StatusUnprocessableEntity, ErrInvalidGender.Error()
+	case errors.Is(err, ErrInavlidVacancy):
+		return http.StatusUnprocessableEntity, ErrInavlidVacancy.Error()
+	case errors.Is(err, ErrInvalidTitle):
+		return http.StatusUnprocessableEntity, ErrInvalidTitle.Error()
+
 	default:
 		return http.StatusInternalServerError, "default internal server error"
 	}
